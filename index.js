2fb01f090dad9b8dba98b70052ce8ad02b108585
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-11-08 13:00:21 +0530

    minor update

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post("/whatsapp-webhook", (req, res) => {
     res.status(200).end();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
